12ced32c167dc1c42dd6df736d67de50c715ae95
Marina Ortiz <a329577@uach.mx>
2021-03-16 15:50:01 -0700

    Se agrega el proyecto con el punto 1

---
+++ proyectobash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#primer punto
+
+	case $1 in
+                -t)
+                echo "Bienvenido a la guia rápida de Agile, para continuar seleccione un tema:"
+                echo "1.SCRUM"
+                echo "2. X.P"
+                echo "3. Kanban"
+                echo "4. Crystal"
+                        ;;
+                -a)
+                echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
+                echo "1. Cascada"
+                echo "2. Espiral"
+                echo "3. Modelo V"
+                        ;;
+                *)
+                echo "Parametro no valido, vuelva a ejecutar"
+		echo "Los parametros validos son: <-t> Agile, <-a> Metodologías tradicionales"
+			;;
+        esac
+
