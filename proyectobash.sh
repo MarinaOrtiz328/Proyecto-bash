5efe581952c77ec7e8d3f87e102e88359f8d2fc8
Daniel Lopez <a315120@uach.mx>
2021-03-18 00:54:17 -0700

    corrigiendo primeros bugs

--- proyectobash.sh
+++ proyectobash.sh
@@ -4,34 +4,34 @@
 
 #segundo punto
 
-seleccionarA (){
-    PS3="Usted esta en la sección $1, seleccione la opción que desea utilizar."
+seleccionar (){
+    PS3="Usted esta en la sección $1, seleccione la opción que desea utilizar: "
     select opt in agregar buscar eliminar leer; do
     case $opt in
         agregar)
-            read -p "Escribe el concepto nuevo" nuevo_nombre
-            read -p "Escribe su definición (sin saltos de linea)" nueva_definicion
+            read -p "Escribe el concepto nuevo: " nuevo_nombre
+            read -p "Escribe su definición (sin saltos de linea): " nueva_definicion
             echo "[$nuevo_nombre] $nueva_definicion" >> "$1.inf"
         ;;
         buscar) 
-            read -p "Inserte el concepto a buscar" concepto
-            grep "$concepto" "$1.txt"
+            read -p "Inserte el concepto a buscar: " concepto
+            grep "$concepto" "$1.inf"
         ;;
         eliminar)
-            read -p "Escriba el concepto a eliminar" concepto
-            n_linea=$(grep -n -m 1 is text.txt |sed  's/\([0-9]*\).*/\1/')
+            read -p "Escriba el concepto a eliminar: " concepto
+            n_linea=$(grep -n -m 1 is $1.inf |sed  's/\([0-9]*\).*/\1/')
             
             #código de eliminado:
-            ed -s hibro.txt <<!
+            ed -s hibro.inf <<!
             $n_linea d
             w
 !
             #final 
             echo "eliminado"
-
             ;;
         leer)
             cat "$1.inf"
+            ;;
         quit)
             break;;
         *) 
@@ -40,22 +40,50 @@ seleccionarA (){
     done
 }
 
-seleccionar(){
+reinicio(){
+    PS3="Quieres regresar al menu anterior o terminar la ejecucion?: "
+    select opt in regresar terminar; do
+    case $opt in
+        regresar)
+            read -p "Escribe el concepto nuevo: " nuevo_nombre
+            read -p "Escribe su definición (sin saltos de linea): " nueva_definicion
+            echo "[$nuevo_nombre] $nueva_definicion" >> "$1.inf"
+        ;;
+        terminar)
+            break;;
+        *) 
+            echo "Invalid option $REPLY";;
+    esac
+    done
+}
+
+seleccionarA(){
     case $1 in
         1)
-            seleccionarA SCRUM;;
+            seleccionar SCRUM;;
         2)
-            seleccionarA X.P;;
+            seleccionar X.P;;
         3)
-            seleccionarA Kanaban;;
+            seleccionar Kanaban;;
         4)
-            seleccionarA Crystal;;
+            seleccionar Crystal;;
         *)
             echo "Parametro no valido, vuelva a ejecutar"
     esac
 }
 
-
+seleccionarB(){
+    case $1 in
+        1)
+            seleccionar CASCADA;;
+        2)
+            seleccionar Espiral;;
+        3)
+            seleccionar ModeloV;;
+        *)
+            echo "Parametro no valido, vuelva a ejecutar"
+    esac
+}
 
 #primer punto
 
@@ -67,7 +95,7 @@ case $1 in
         echo "3. Kanban"
         echo "4. Crystal"
         read -p "Selecciona una opcion: " n
-        seleccionar $n
+        seleccionarA $n
                 ;;
         -t)
         echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
@@ -75,6 +103,7 @@ case $1 in
         echo "2. Espiral"
         echo "3. Modelo V"
         read -p "Selecciona una opcion: " n
+        seleccionarB $n
                 ;;
         *)
         echo "Parametro no valido, vuelva a ejecutar"
